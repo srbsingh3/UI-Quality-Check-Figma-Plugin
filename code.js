4731c60bee2c43ab6dafee746b82d50053de3abd
srbsingh3 <32899793+srbsingh3@users.noreply.github.com>
2021-10-09 00:45:19 +0200

    increased width and height

--- code.js
+++ code.js
@@ -4,7 +4,7 @@
 // You can access browser APIs in the <script> tag inside "ui.html" which has a
 // full browser environment (see documentation).
 // This shows the HTML page in "ui.html".
-figma.showUI(__html__);
+figma.showUI(__html__, { width: 600, height: 700, title: "UI Quality Check" });
 // Calls to "parent.postMessage" from within the HTML page will trigger this
 // callback. The callback will be passed the "pluginMessage" property of the
 // posted message.
--- code.ts
+++ code.ts
@@ -6,7 +6,7 @@
 // full browser environment (see documentation).
 
 // This shows the HTML page in "ui.html".
-figma.showUI(__html__);
+figma.showUI(__html__, { width: 600, height: 700, title: "UI Quality Check" });
 
 // Calls to "parent.postMessage" from within the HTML page will trigger this
 // callback. The callback will be passed the "pluginMessage" property of the
