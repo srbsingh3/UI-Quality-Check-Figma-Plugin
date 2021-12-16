d0c5ca591fc05bb21d1833cfe3e9dc36943eb244
srbsingh3 <32899793+srbsingh3@users.noreply.github.com>
2021-12-16 17:21:36 +0100

    CSS is sorted

--- code.js
+++ code.js
@@ -4,25 +4,25 @@
 // You can access browser APIs in the <script> tag inside "ui.html" which has a
 // full browser environment (see documentation).
 // This shows the HTML page in "ui.html".
-figma.showUI(__html__, { width: 600, height: 700, title: "UI Quality Check" });
+figma.showUI(__html__, { width: 989, height: 400, title: "UI Quality Check" });
 // Calls to "parent.postMessage" from within the HTML page will trigger this
 // callback. The callback will be passed the "pluginMessage" property of the
 // posted message.
-figma.ui.onmessage = msg => {
+figma.ui.onmessage = (msg) => {
     // One way of distinguishing between different types of messages sent from
     // your HTML page is to use an object with a "type" property like this.
-    if (msg.type === 'create-rectangles') {
-        const nodes = [];
-        for (let i = 0; i < msg.count; i++) {
-            const rect = figma.createRectangle();
-            rect.x = i * 150;
-            rect.fills = [{ type: 'SOLID', color: { r: 1, g: 0.5, b: 0 } }];
-            figma.currentPage.appendChild(rect);
-            nodes.push(rect);
-        }
-        figma.currentPage.selection = nodes;
-        figma.viewport.scrollAndZoomIntoView(nodes);
-    }
+    // if (msg.type === 'create-rectangles') {
+    //   const nodes: SceneNode[] = [];
+    //   for (let i = 0; i < msg.count; i++) {
+    //     const rect = figma.createRectangle();
+    //     rect.x = i * 150;
+    //     rect.fills = [{type: 'SOLID', color: {r: 1, g: 0.5, b: 0}}];
+    //     figma.currentPage.appendChild(rect);
+    //     nodes.push(rect);
+    //   }
+    //   figma.currentPage.selection = nodes;
+    //   figma.viewport.scrollAndZoomIntoView(nodes);
+    // }
     // Make sure to close the plugin when you're done. Otherwise the plugin will
     // keep running, which shows the cancel button at the bottom of the screen.
     figma.closePlugin();
