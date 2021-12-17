89a22a2cf741e9967d3d151ea9a7d66ea18f7fa9
srbsingh3 <32899793+srbsingh3@users.noreply.github.com>
2021-12-17 17:48:16 +0100

    UI code complete

--- code.ts
+++ code.ts
@@ -12,22 +12,5 @@ figma.showUI(__html__, { width: 989, height: 700, title: "UI Quality Check" });
 // callback. The callback will be passed the "pluginMessage" property of the
 // posted message.
 figma.ui.onmessage = (msg) => {
-	// One way of distinguishing between different types of messages sent from
-	// your HTML page is to use an object with a "type" property like this.
-	// if (msg.type === 'create-rectangles') {
-	//   const nodes: SceneNode[] = [];
-	//   for (let i = 0; i < msg.count; i++) {
-	//     const rect = figma.createRectangle();
-	//     rect.x = i * 150;
-	//     rect.fills = [{type: 'SOLID', color: {r: 1, g: 0.5, b: 0}}];
-	//     figma.currentPage.appendChild(rect);
-	//     nodes.push(rect);
-	//   }
-	//   figma.currentPage.selection = nodes;
-	//   figma.viewport.scrollAndZoomIntoView(nodes);
-	// }
-
-	// Make sure to close the plugin when you're done. Otherwise the plugin will
-	// keep running, which shows the cancel button at the bottom of the screen.
 	figma.closePlugin();
 };
